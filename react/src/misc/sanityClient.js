f621a09b1c31064824d9d3f846340a9b4cec688c
Rebeckawestborg <rwestborg@gmail.com>
2026-05-11 11:54:56 +0200

    la inn prosjektId og token i sanityClient.js

--- react/src/misc/sanityClient.js
+++ react/src/misc/sanityClient.js
@@ -1,9 +1,9 @@
 import { createClient } from '@sanity/client'
 
-const projectId = 'YOUR_PROJECT_ID'
+const projectId = 'd6ptjl8a'
 const dataset = 'production'
 const apiVersion = '2026-04-20' // use current date or a fixed API version
-const token = 'YOUR_API_TOKEN'
+const token = 'skp4SyanmfhSiwArjVJH1X7yQ7Nbt0HZ3vV7TT0UTPA4gPQhihKrDNBbzb6xeRJV3x3BGxkG8Vy3MC00wG38PJZfy5YuYNaRSIyQa8Ug6MMmLoLMfrhzk0Taum1yFK0GoCybaFgcLYJVloqzxZRG5C0Hfrquxpkx9zX71oL0Wx12khDv7WZW'
 
 export const sanityClient = createClient({
   projectId,
